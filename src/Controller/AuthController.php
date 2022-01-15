566eec3b7a5abe9e2d1cc00a027ac07e7296cc36
zuarel <bubl174rus@gmail.com>
2022-01-15 15:23:04 +0300

    refactor AuthController

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,58 +2,62 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Service\AuthService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthController extends AbstractController
 {
 
-    private $doctrine;
+    private AuthService $service;
 
-    public function __construct(ManagerRegistry $doctrine)
+    public function __construct(AuthService $service)
     {
-        $this->doctrine = $doctrine;
+        $this->service = $service;
     }
 
     /**
      * @Route("/api/auth/register", name="auth", methods={"POST"})
      */
-    public function register(UserPasswordHasherInterface $password_hasher, Request $request): Response
+    public function register(Request $request): Response
     {
+        try {
+            $content = json_decode($request->getContent(), true);
 
-        $content = json_decode($request->getContent(), true);
+            $username = $content['username'];
+            $password = $content['password'];
 
-        $username = $content['username'];
-        $password = $content['password'];
+            $user = $this->service->register($username, $password);
 
-        $repository = $this->doctrine->getRepository(User::class);
-
-        $existed_user = $repository->findOneBy(['email' => $username]);
-
-        if($existed_user) {
             return $this->json([
-                'message' => 'Пользователь с такой почтой уже существует'
-            ], 500);
+                'id' => $user->getId(),
+                'email' => $user->getUserIdentifier()
+            ]);
+        } catch (\Error $error) {
+            return $this->json(['message' => $error->getMessage()], 500);
         }
+    }
 
-        $user = new User();
-        $user->setEmail($username);
+    /**
+     * @Route("/api/auth/login", methods={"POST"})
+     */
+    public function login(Request $request): Response
+    {
+        try {
+            $content = json_decode($request->getContent(), true);
 
-        $hashed_password = $password_hasher->hashPassword($user, $password);
-        $user->setPassword($hashed_password);
-        $this->doctrine->getManager()->persist($user);
-        $this->doctrine->getManager()->flush();
+            $username = $content['username'];
+            $password = $content['password'];
 
+            $token = $this->service->login($username, $password);
 
-        return $this->json([
-            'status' => 'ok',
-            'id' => $user->getId(),
-            'email' => $user->getUserIdentifier()
-        ]);
+            return $this->json([
+                'token' => $token
+            ]);
+        } catch (\Error $error) {
+            return $this->json(['message' => $error->getMessage()], 500);
+        }
     }
 }
